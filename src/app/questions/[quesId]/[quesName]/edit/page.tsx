74ed7cbde7d86f5e9ba6be181530d48689d75744
iamsidverma26 <20BCS9848@cuchd.in>
2025-01-24 00:50:07 +0530

    commenting

--- src/app/questions/[quesId]/[quesName]/edit/page.tsx
+++ src/app/questions/[quesId]/[quesName]/edit/page.tsx
@@ -1,18 +1,21 @@
-import { db, questionCollection } from "@/models/name";
-import { databases } from "@/models/server/config";
-import React from "react";
-import EditQues from "./EditQues";
+// import { db, questionCollection } from "@/models/name";
+// import { databases } from "@/models/server/config";
+// import React from "react";
+// import EditQues from "./EditQues";
 
-const Page = async ({
-  params: rawParams,
-}: {
-  params: Promise<{ quesId: string; quesName: string }>;
-}) => {
-  const params = await rawParams;
-  const { quesId } = params;
-  const question = await databases.getDocument(db, questionCollection, quesId);
+// const Page = async ({
+//   params: rawParams,
+// }: {
+//   params: Promise<{ quesId: string; quesName: string }>;
+// }) => {
+//   const params = await rawParams;
+//   console.log(params.quesId);
+//   const { quesId } = params;
+//   console.log(quesId);
 
-  return <EditQues question={question} />;
-};
+//   const question = await databases.getDocument(db, questionCollection, quesId);
 
-export default Page;
+//   return <EditQues question={question} />;
+// };
+
+// export default Page;
